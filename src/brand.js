7bc28cd727f8c2f2e4e26512f9e3c14e3678b979
Shakshi2904 <132297826+Shakshi2904@users.noreply.github.com>
2024-06-28 07:57:47 +0530

    Update brand.js

--- src/brand.js
+++ src/brand.js
@@ -3,15 +3,9 @@ import './brand.css';
 function Brand() {
   return (
     <div className='brandpage'>
-        <h1 className="hoverable-element">ESTABLISHING INDIA’S LARGEST SMART RETAIL</h1>
-        <h1 className="hoverable-element">NETWORK WITH INTELLIGENT VENDING</h1>
-        <h1 className="hoverable-element">MACHINES</h1>
-        <div className='para'>
-            <p className="hoverable-element">Creating a platform for brands and retailers to offer sub-₹99</p>
-            <p className="hoverable-element">food instantly with no delivery cost through intelligent retail</p>
-            <p className="hoverable-element">solutions such as vending machines, smart fridges, and</p>
-            <p className="hoverable-element">mobile retail units.</p>
-        </div>
+        <div id='brand' className="hoverable-element">
+                <h1 className="hoverable-element">MAKING INDIA FUTURE READY WITH UNMANNED QUICK BUYING SOLUTIONS</h1>
+                <p className="hoverable-element"> Enabling last mile distribution through retailers & franchise partners through intelligent retail offerings.</p></div>
         <div className='brandPic'>
             <div className='storeGroup'>
             <img className="hoverable-element" src='store.png' alt='hi'/>
