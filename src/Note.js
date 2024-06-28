abecc7b7e9fcb5a4710294aa94dd902321a2f9a7
Shakshi2904 <132297826+Shakshi2904@users.noreply.github.com>
2024-06-28 07:58:18 +0530

    Update Note.js

--- src/Note.js
+++ src/Note.js
@@ -7,7 +7,7 @@ function Note(props){
             <img className="hoverable-element" src='feedback.png' alt='not available' height={'50vh'} width={'50vw'}/>
             <div id="name">
             <h2 className="hoverable-element">Name here</h2>
-            Designation
+            <h5>Designation</h5>
             </div>
         </div>
         <div>
@@ -15,7 +15,7 @@ function Note(props){
         I can't express enough how much this image-based damage assessment tool has transformed my experience with insurance claims. Its ability to swiftly categorizet and assess damages in real-time not only expedited the entire process but also provided me with a level of confidence in the accuracy of the assessment. The seamless integration into existing insurance systems made the entire experience hassle-free..
         </p>
         </div>
-        <span className="fa fa-star checked"></span>
+        <span className="fa fa-star checked start1"></span>
       <span className="fa fa-star checked"></span>
       <span className="fa fa-star checked"></span>
       <span className="fa fa-star checked"></span>
