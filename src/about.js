c0fbacc9e5dbe9a66e4e42c668458e0740d42c31
Shakshi2904 <132297826+Shakshi2904@users.noreply.github.com>
2024-07-11 23:07:31 +0530

    Update about.js

--- src/about.js
+++ src/about.js
@@ -4,8 +4,7 @@ function About(){
         <div>
             <div id='about'>
                 <div  className='ourServicePage'><h1>ABOUT US!</h1></div>
-                <p> For 24Buy7 necessity was the very virtue of its existence. When a group of young students pursuing management studies felt the craving to binge on snacks and beverages but couldn't despite the in campus stall being right in front of them but unfortunately closed during the day. The dissatisfaction made them realize the need for an Unmanned Retail ATM that could dispense snacks and beverages. This gave birth to the idea of building a venture around Vending Machines and catering to on-the-go needs of the consumers anytime and everytime. 
-                <br></br> <br></br>Our first customer was the one which fed us the idea, our Alma Mater. Since then there has been no looking back and that is how 24Buy7 took off. Over time, we have catered to customers across various domains - Corporate Offices, Educational Institutions & Campuses, Residential Complexes.</p>
+                <p> Welcome to 24buy7, your trusted partner in delivering top-notch snack and beverage vending solutions. We cater to a diverse range of environments including corporate offices, schools, colleges, and industrial settings. Our goal is to ensure that you have access to your favorite snacks and drinks whenever you need them, 24/7.</p>
             </div>
             <div id='aboutvideo'>
             <video autoplay>
